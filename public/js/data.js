846fbcf5b0cd84989884b7e08496ff5ec032749f
Loris Pandaru <loris.buchelet@gmail.com>
2024-07-20 19:18:21 +0200

    update on design

--- public/js/data.js
+++ public/js/data.js
@@ -13,14 +13,13 @@ const buttons = [
     ['readBio', 'Read my bio', 'Lire ma bio'],
     ['findMore', 'Find out more', 'Voir plus'],
     ['visitWebsite', 'Visit website', 'Visiter le site'],
-    ['submitForm', 'Submit', 'Envoyer'],
-    ['emailSend', 'Send an e-mail', 'Envoyer un mail']
+    ['emailSend', 'Send me an e-mail', 'Envoyer un mail']
 ];
 
 const titles = [
     ['h1Intro',
-    `I'm Loris Buchelet, former English teacher and Web Developer in training.`,
-    `Je suis Loris Buchelet, ex-professeur d'anglais et Développeur Web en formation`
+    `I'm Loris Buchelet, <br>former English teacher<br> and Web Developer in training.`,
+    `Je suis Loris Buchelet, <br>ex-professeur d'anglais<br> et Développeur Web en formation`
     ],
 
     ['h2Intro', 
@@ -61,4 +60,20 @@ const titles = [
     ],
 ];
 
+const projects = [
+    [   'Project 1: Arcadia Zoo',
+        'Projet 1: Zoo Arcadia',
+        'This project, designed for my Study graduate developer training, implements a zoo website with a login access to zoo employees.',
+        'Ce projet, réalisé pour ma certification Studi Graduate Developpeur, donne accès à un site web pour un zoo accessible à des employés.'
+    ],
+    [   'Project 2: Teacher Tool',
+        'Projet 2 : Teacher Tool',
+        'This project, designed for my Study graduate developer training, implements a system for teachers to add and access students\' marks',
+        'Ce projet, réalisé pour ma certification Studi Graduate Developpeur, permet aux enseignants d\'ajouter des compétences des élèves pour suivre leurs classes.'
+    ],
+    [   'Project 3: Shiny Hunting',
+        'This project, designed for my Studi graduate developer training, implements a checking list for Shiny Pokemon hunters.',
+        'Ce projet, réalisé pour ma certification Studi Graduate Developpeur, donne accès à un check list des Pokémon chromatiques.'
+    ]
+]
 
